a9f8f8a7b0f50aa9eaf4c92e6e7d7535176f0bc2
Fady Nabil Samy <fadynabil96@gmail.com>
2017-10-02 18:29:09 +0200

    created project for the first time

---
+++ cmake-build-debug/CMakeFiles/Shell_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shell_Project.dir/main.c.o"
+  "Shell_Project.pdb"
+  "Shell_Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Shell_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
